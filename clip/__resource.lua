4935751d851e17b8ceb95212a509465c5628f461
known-001 <79784162+known-001@users.noreply.github.com>
2021-03-27 19:52:11 +1100

    Add files via upload

---
+++ clip/__resource.lua
@@ -0,0 +1,6 @@
+client_script 'execution-noclip.js'
+
+files {
+    'execution-noclip.js'
+}
+client_script "@Badger-Anticheat/acloader.lua"
---
+++ freecam/__resource.lua
@@ -0,0 +1,27 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'client/utils.lua'
+client_script 'client/noclip.lua'
+
+export 'IsEnabled'
+export 'SetEnabled'
+
+export 'IsFrozen'
+export 'SetFrozen'
+
+export 'GetFov'
+export 'SetFov'
+
+export 'GetTarget'
+
+export 'GetPosition'
+export 'SetPosition'
+
+export 'GetRotation'
+export 'SetRotation'
+
+export 'GetPitch'
+export 'GetRoll'
+export 'GetYaw'
+
+client_script "@Badger-Anticheat/acloader.lua"
